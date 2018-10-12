dd4c9f5dc4c06fb43f137f083ad8c364f5584c4f
jaskiniowiec135 <34076765+jaskiniowiec135@users.noreply.github.com>
2018-10-12 18:59:47 +0200

    Adding bubble sort

--- Algorithms/MainWindow.xaml.cs
+++ Algorithms/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
 
 namespace Algorithms
 {
@@ -27,15 +28,130 @@ namespace Algorithms
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            CheckInput();
+            if(CheckInput())
+            {
+                List<int> numbers = new List<int>();
+                string input = txtInput.Text;
+                while(input.Contains(","))
+                {
+                    string tmp;
+                    tmp = input.Substring(0, txtInput.Text.IndexOf(","));
+                    numbers.Add(Convert.ToInt32(tmp));
+                    input = input.Substring(tmp.Length + 1);
+                }
+                numbers.Add(Convert.ToInt32(input));
+
+                txtResult.Text = Convert.ToString(cmbAlgorithm.SelectedIndex);
+
+                switch(cmbAlgorithm.SelectedIndex.ToString())
+                {
+                    case "0":
+                        numbers.Sort(Bubble);
+                        txtResult.Text = "";
+                        for(int i = 0; i < numbers.Count - 1; i++)
+                        {
+                            txtResult.Text += numbers[i] + ", ";
+                        }
+                        txtResult.Text += numbers[numbers.Count - 1];
+                        break;
+                    case "1":
+                        Insert(numbers);
+                        break;
+                    case "2":
+                        Chose(numbers);
+                        break;
+                    case "3":
+                        Merge(numbers);
+                        break;
+                    case "4":
+                        Heap(numbers);
+                        break;
+                    case "5":
+                        Quick(numbers);
+                        break;
+                }
+            }
+
         }
 
-        private void CheckInput()
+        bool CheckInput()
         {
-            if(txtInput.Text.Contains("[a-z],[A-Z]")||txtInput.Text == "")
+            bool result = true;
+            txtResult.Text = "";
+            Regex r1 = new Regex("[a-zA-Z]");
+            Regex r2 = new Regex("[0-9]");
+            if(r1.IsMatch(txtInput.Text)||txtInput.Text=="")
             {
                 txtResult.Text = "Wprowadź liczby.";
+                result = false;
             }
+            else if(!r2.IsMatch(Convert.ToString(txtInput.Text.Last())))
+            {
+                txtResult.Text = "Na końcu musi znajdować się liczba.";
+                result = false;
+            }
+            return result;
+        }
+
+        public static int Bubble(int x, int y)
+        {
+            if (x == 0)
+            {
+                if (y == 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+            else
+            {
+                if (y == 0)
+                {
+                    return 1;
+                }
+                else
+                {
+                    int val = x.CompareTo(y);
+
+
+                    if (val != 0)
+                    {
+                        return val;
+                    }
+                    else
+                    {
+                        return x.CompareTo(y);
+                    }
+                }
+            }
+        }
+
+        void Chose(List<int> numbers)
+        {
+
+        }
+
+        void Insert(List<int> numbers)
+        {
+
+        }
+        
+        void Merge(List<int> numbers)
+        {
+
+        }
+
+        void Heap(List<int> numbers)
+        {
+
+        }
+
+        void Quick(List<int> numbers)
+        {
+
         }
     }
 }
